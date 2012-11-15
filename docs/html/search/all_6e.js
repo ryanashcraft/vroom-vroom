29d366f3692af5edf3047898483f13dcedf18ce8
Ryan Ashcraft <ryanashcraft@gmail.com>
2012-11-15 08:35:59 -0500

    Document Date.

---
+++ docs/html/search/all_6e.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['now',['now',['../classvv_1_1_date.html#a7bbeae231f3cc86a19c82456a9eec15e',1,'vv::Date']]]
+];
--- include/date.h
+++ include/date.h
@@ -8,8 +8,16 @@
 namespace vv {
 	using namespace std;
 
+	/**
+	 * Represents date and time.
+	 */
 	class Date {
 	public:
+		/**
+		 * Static function for a generating a formatted string from the current time.
+		 * @param  format the date format for the generated string
+		 * @return        the generated formatted string for the current time
+		 */
 		static string now(const string& format);
 	};
 }
