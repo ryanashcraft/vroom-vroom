80468f2eb9185a8d275eb37efac365ee8642d9ef
Ryan Ashcraft <ryanashcraft@gmail.com>
2012-11-19 16:01:25 -0500

    Document TextInterpreter.

--- docs/html/search/functions_74.js
+++ docs/html/search/functions_74.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['textinterpreter',['TextInterpreter',['../classvv_1_1_text_interpreter.html',1,'vv']]],
   ['textinterpreter',['TextInterpreter',['../classvv_1_1_text_interpreter.html#af22e0fef7ef9b61456063e49f10ec74e',1,'vv::TextInterpreter']]]
 ];
--- include/text_interpreter.h
+++ include/text_interpreter.h
@@ -10,9 +10,22 @@
 namespace vv {
 	using namespace std;
 
+	/**
+	 * Interprets a file as a text file for an HTTP response.
+	 */
 	class TextInterpreter : public vv::FileInterpreter {
 	public:
+		/**
+		 * Creates a text file interpreter with a file path and a mime type string.
+		 * @param  path the path of the file to open
+		 * @param  mime the mime type string
+		 */
 		explicit TextInterpreter(const string& path, const string& mime="text/plain");
+
+		/**
+		 * Returns the string read from the text file.
+		 * @return the string read from the text file
+		 */
 		string interpret();
 	};
 }
